ba13f431b72025377ab02db7b0f6e0e22d748b11
Abhi1001001 <av159593@gmail.com>
2025-12-07 15:15:14 +0530

    resume updation

--- abhishek-portfolio-3.0/src/Componenets/Navbar.jsx
+++ abhishek-portfolio-3.0/src/Componenets/Navbar.jsx
@@ -68,13 +68,13 @@ export default function Navbar() {
   // download pdf function------------------------>
   const downloadResume = () => {
     fetch(
-      "https://drive.google.com/file/d/19dcdbyc698htNml1AdDKsMfy0_2e9tgr/view?usp=drive_link"
+      "https://drive.google.com/file/d/1Bx4-VMPw54NfynMRRkVRcdhE-Upe8AwT/view?usp=sharing"
     ).then((response) => {
       response.blob().then((blob) => {
         const fileURL = window.URL.createObjectURL(blob);
         let alink = document.createElement("a");
         alink.href = fileURL;
-        alink.download = "Abhishek_Vishwakarma_Resume.pdf";
+        alink.download = "Abhishek_vishwakarma_resume.pdf";
         alink.click();
       });
     });
@@ -90,159 +90,160 @@ export default function Navbar() {
   return (
     <>
       {/* navbar section ---------------------------> */}
-      <main className="">
-        <nav className="backdrop-blur-lg max-w-7xl m-auto flex justify-between items-center px-4 font-concert text-xl sticky top-0 dark:text-white">
-          <Link to="/" className="w-14 outline-none">
-            <LogoIcon className="text-[#6ee5ce]" />
+
+      <nav className="backdrop-blur-lg max-w-7xl m-auto flex justify-between items-center px-4 font-concert text-xl sticky top-0 dark:text-white">
+        <Link to="/" className="w-14 outline-none">
+          <LogoIcon className="text-[#6ee5ce]" />
+        </Link>
+        {/* desktop menu ----------> */}
+        <div className="gap-5 hidden items-center sm:flex">
+          <Link className="nav-text" to="/">
+            Home
           </Link>
-          {/* desktop menu ----------> */}
-          <div className="gap-5 hidden items-center sm:flex">
-            <Link className="nav-text" to="/">
-              Home
-            </Link>
-            <Link className="nav-text" to="/skills">
-              Skills
-            </Link>
-            <Link className="nav-text" to="/experience">
-              Experience
-            </Link>
-            <Link className="nav-text" to="/education">
-              Education
-            </Link>
-            <Link className="nav-text" to="/projects">
-              Projects
-            </Link>
-            <Link className="nav-text" to="/contact-us">
-              Contact Us
-            </Link>
-            <button onClick={toggleDarkMode} className="nav-text">
+          <Link className="nav-text" to="/skills">
+            Skills
+          </Link>
+          <Link className="nav-text" to="/experience">
+            Experience
+          </Link>
+          <Link className="nav-text" to="/education">
+            Education
+          </Link>
+          <Link className="nav-text" to="/projects">
+            Projects
+          </Link>
+          <Link className="nav-text" to="/contact-us">
+            Contact Us
+          </Link>
+          <button onClick={toggleDarkMode} className="nav-text">
+            {darkMode ? (
+              <SunIcon className="sm:w-6 text-black cursor-pointer dark:text-white" />
+            ) : (
+              <MoonIcon className="sm:w-6 text-black cursor-pointer" />
+            )}
+          </button>
+          <button
+            className="rounded-full lg:px-2 sm:px-2 px-2 lg:py-1 sm:py-[6px] py-2 duration-300 text-white bg-[#59d0b4] flex active:scale-110 active:duration-0 lg:text-base sm:text-sm font-josefin"
+            onClick={downloadResume}
+          >
+            CV
+            <DownloadIcon className="w-5" />
+          </button>
+        </div>
+        {/* menu icon -------------> */}
+        <div className="relative sm:hidden block">
+          <div className="flex items-center gap-2">
+            <button onClick={toggleDarkMode}>
               {darkMode ? (
-                <SunIcon className="sm:w-6 text-black cursor-pointer dark:text-white" />
+                <SunIcon className="w-6 text-black dark:text-white cursor-pointer" />
               ) : (
-                <MoonIcon className="sm:w-6 text-black cursor-pointer" />
+                <MoonIcon className="w-6 text-black dark:text-white cursor-pointer" />
               )}
             </button>
+
             <button
-              className="rounded-full lg:px-2 sm:px-2 px-2 lg:py-1 sm:py-[6px] py-2 duration-300 text-white bg-[#59d0b4] flex active:scale-110 active:duration-0 lg:text-base sm:text-sm font-josefin"
+              className="rounded-full lg:px-2 px-2 sm:py-[6px] sm:h-auto h-8 sm:hidden duration-300 text-white bg-[#59d0b4] flex active:scale-110 active:duration-0 lg:text-base text-sm items-center"
               onClick={downloadResume}
             >
               CV
               <DownloadIcon className="w-5" />
             </button>
+
+            <button onClick={mobileMenuToggle}>
+              {isMobileMenuOpen ? <X size={28} /> : <Menu size={28} />}
+            </button>
           </div>
-          {/* menu icon -------------> */}
-          <div className="relative sm:hidden block">
-            <div className="flex items-center gap-2">
-              <button onClick={toggleDarkMode}>
-                {darkMode ? (
-                  <SunIcon className="w-6 text-black dark:text-white cursor-pointer" />
-                ) : (
-                  <MoonIcon className="w-6 text-black dark:text-white cursor-pointer" />
-                )}
-              </button>
-              <button
-                className="rounded-full lg:px-2 px-2 sm:py-[6px] sm:h-auto h-8 sm:hidden duration-300 text-white bg-[#59d0b4] flex active:scale-110 active:duration-0 lg:text-base text-sm items-center"
-                onClick={downloadResume}
+          {isMobileMenuOpen && (
+            <div className="w-1/2 fixed top-20 right-0 h-screen flex flex-col items-center justify-start space-y-8 text-2xl font-semibold z-50 dark:border-white bg-[#e3e3e3] dark:bg-[#121212]">
+              <Link
+                to="/home"
+                onClick={mobileMenuToggle}
+                className="hover:text-teal-400 cursor-pointer"
               >
-                CV
-                <DownloadIcon className="w-5" />
-              </button>
-              <button onClick={mobileMenuToggle}>
-                {isMobileMenuOpen ? <X size={28} /> : <Menu size={28} />}
-              </button>
+                Home
+              </Link>
+              <Link
+                to="/skills"
+                onClick={mobileMenuToggle}
+                className="hover:text-teal-400 cursor-pointer"
+              >
+                Skills
+              </Link>
+              <Link
+                to="/experience"
+                onClick={mobileMenuToggle}
+                className="hover:text-teal-400 cursor-pointer"
+              >
+                Experience
+              </Link>
+              <Link
+                to="/education"
+                onClick={mobileMenuToggle}
+                className="hover:text-teal-400 cursor-pointer"
+              >
+                Education
+              </Link>
+              <Link
+                to="/projects"
+                onClick={mobileMenuToggle}
+                className="hover:text-teal-400 cursor-pointer"
+              >
+                Projects
+              </Link>
+              <Link
+                to="/contact-us"
+                onClick={mobileMenuToggle}
+                className="hover:text-teal-400 cursor-pointer"
+              >
+                Contact Us
+              </Link>
             </div>
-            {isMobileMenuOpen && (
-              <div className="w-1/2 fixed top-20 right-0 h-screen flex flex-col items-center justify-start space-y-8 text-2xl font-semibold z-50 dark:border-white bg-[#e3e3e3] dark:bg-[#121212]">
-                <Link
-                  to="/home"
-                  onClick={mobileMenuToggle}
-                  className="hover:text-teal-400"
-                >
-                  Home
-                </Link>
-                <Link
-                  to="/skills"
-                  onClick={mobileMenuToggle}
-                  className="hover:text-teal-400"
-                >
-                  Skills
-                </Link>
-                <Link
-                  to="/experience"
-                  onClick={mobileMenuToggle}
-                  className="hover:text-teal-400"
-                >
-                  Experience
-                </Link>
-                <Link
-                  to="/education"
-                  onClick={mobileMenuToggle}
-                  className="hover:text-teal-400"
-                >
-                  Education
-                </Link>
-                <Link
-                  to="/projects"
-                  onClick={mobileMenuToggle}
-                  className="hover:text-teal-400"
-                >
-                  Projects
-                </Link>
-                <Link
-                  to="/contact-us"
-                  onClick={mobileMenuToggle}
-                  className="hover:text-teal-400"
-                >
-                  Contact Us
-                </Link>
+          )}
+        </div>
+      </nav>
+
+      {/* contect icon section ---------------------> */}
+      <section className="fixed top-1/4 left-[2vw] flex flex-col sm:gap-5 gap-3">
+        <Link
+          target="_blank"
+          to="https://wa.me/7235047144?text=Hello, I find you by your portfolio"
+        >
+          <WhatsappIcon className="contect-icon lg:w-7 sm:w-6 w-5 dark:text-white" />
+        </Link>
+        <Link target="_blank" to="mailto:av159593@gmail.com">
+          <EmailIcon className="contect-icon lg:w-7 sm:w-6 w-5 dark:text-white" />
+        </Link>
+        <Link
+          target="_blank"
+          to="https://www.linkedin.com/in/abhishek-vishwakarma-127b66227/"
+        >
+          <LinkedinIcon className="contect-icon lg:w-7 sm:w-6 w-5 dark:text-white" />
+        </Link>
+        <Link target="_blank" to="tel:+917235047144">
+          <PhoneIcon className="contect-icon lg:w-7 sm:w-6 w-5 dark:text-white" />
+        </Link>
+      </section>
+
+      {/* chat icon section ---------------------> */}
+      <section className="fixed md:bottom-10 bottom-7 md:right-12 right-8 flex gap-3 z-10 justify-center items-center">
+        <div className="dark:bg-white dark:text-black bg-[#121212] text-white border border-[#59d0b4] text-center rounded-xl h-fit sm:px-3 px-2 py-1 sm:text-base text-sm font-josefin">
+          Chat with us
+        </div>
+        <div className="dark:bg-white bg-[#121212] sm:w-4 w-3 sm:h-4 h-3 border-r border-t border-[#59d0b4] rotate-45 absolute sm:right-[32px] right-[30px] bottom-[9px] z-10"></div>
+        <div>
+          <Popup
+            trigger={<ChatIcon className="md:w-6 w-5 text-[#59d0b4]" />}
+            modal
+            nested
+          >
+            {(close) => (
+              <div className="modal">
+                <ChatPopup close={close} />
               </div>
             )}
-          </div>
-        </nav>
-
-        {/* contect icon section ---------------------> */}
-        <section className="fixed top-1/4 left-[2vw] flex flex-col sm:gap-5 gap-3">
-          <Link
-            target="_blank"
-            to="https://wa.me/7235047144?text=Hello, I find you by your portfolio"
-          >
-            <WhatsappIcon className="contect-icon lg:w-7 sm:w-6 w-5 dark:text-white" />
-          </Link>
-          <Link target="_blank" to="mailto:av159593@gmail.com">
-            <EmailIcon className="contect-icon lg:w-7 sm:w-6 w-5 dark:text-white" />
-          </Link>
-          <Link
-            target="_blank"
-            to="https://www.linkedin.com/in/abhishek-vishwakarma-127b66227/"
-          >
-            <LinkedinIcon className="contect-icon lg:w-7 sm:w-6 w-5 dark:text-white" />
-          </Link>
-          <Link target="_blank" to="tel:+917235047144">
-            <PhoneIcon className="contect-icon lg:w-7 sm:w-6 w-5 dark:text-white" />
-          </Link>
-        </section>
-
-        {/* chat icon section ---------------------> */}
-        <section className="fixed md:bottom-10 bottom-7 md:right-12 right-8 flex gap-3 z-10 justify-center items-center">
-          <div className="dark:bg-white dark:text-black bg-[#121212] text-white border border-[#59d0b4] text-center rounded-xl h-fit sm:px-3 px-2 py-1 sm:text-base text-sm font-josefin">
-            Chat with us
-          </div>
-          <div className="dark:bg-white bg-[#121212] sm:w-4 w-3 sm:h-4 h-3 border-r border-t border-[#59d0b4] rotate-45 absolute sm:right-[32px] right-[30px] bottom-[9px] z-10"></div>
-          <div>
-            <Popup
-              trigger={<ChatIcon className="md:w-6 w-5 text-[#59d0b4]" />}
-              modal
-              nested
-            >
-              {(close) => (
-                <div className="modal">
-                  <ChatPopup close={close} />
-                </div>
-              )}
-            </Popup>
-          </div>
-        </section>
-      </main>
+          </Popup>
+        </div>
+      </section>
     </>
   );
 }
